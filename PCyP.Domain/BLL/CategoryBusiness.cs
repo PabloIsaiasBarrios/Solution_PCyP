b9cdea2c3d383163d0b8422e09614cebbfbdda45
PabloIsaiasBarrios <isaias.unpaz33@gmail.com>
2017-08-24 20:29:03 -0300

    init master

---
+++ PCyP.Domain/BLL/CategoryBusiness.cs
@@ -0,0 +1,33 @@
+﻿using PCyP.Domain.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCyP.Domain.BLL
+{
+    public static class CategoryBusiness
+    {
+
+        private static List<Category> _categoryList = new List<Category>();
+
+        /// <summary>
+        /// Descripcion del metodo Add
+        /// </summary>
+        /// <param name="categoria"></param>
+        public static void Add(Category categoria)
+        {
+            _categoryList.Add(categoria);
+        }
+        /// <summary>
+        /// Descripcion del metodo GetCategoryList
+        /// </summary>
+        /// <returns></returns>
+        public static List<Category>  GetCategoryList()
+        {
+            return _categoryList;
+        }
+        
+    }
+}
---
+++ PCyP.WebSite/Controllers/CategoryController.cs
@@ -0,0 +1,93 @@
+﻿using PCyP.Domain.Biz;
+using PCyP.Domain.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PCyP.WebSite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var lista = CategoryBusiness.GetCategoryList();
+            return View(lista);
+        }
+
+        // GET: Category/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        [HttpPost]
+        public ActionResult Create(Category model)
+        {
+            try
+            {
+
+                // TODO: Add insert logic here
+                CategoryBusiness.Add(model);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Category/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Category/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Category/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
