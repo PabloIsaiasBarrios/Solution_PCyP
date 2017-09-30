7720a2911e3a70a644d1b2a5cfba81501a22f02a
Pablo Isaias Barrios <isaias.unpaz33@gmail.com>
2017-09-30 16:42:00 -0300

    Gestor de Estudiantes realizado

---
+++ PCyP.Domain/BLL/CargarDatos.cs
@@ -0,0 +1,68 @@
+﻿using PCyP.Domain.Biz;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCyP.Domain.BLL
+{
+    public class CargarDatos
+    {
+        public CargarDatos()
+        {
+
+        }
+
+        public static void LeerArchivo()
+        {
+            ArrayList apellidos = new ArrayList();
+
+            ArrayList nombre = new ArrayList();
+
+            int counter = 0;
+            string line;
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@AppDomain.CurrentDomain.BaseDirectory + "\\App_Data\\apellido.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                apellidos.Add(line);
+                counter++;
+            }
+
+            file.Close();
+
+
+            counter = 0;
+            file =
+                new System.IO.StreamReader(@AppDomain.CurrentDomain.BaseDirectory + "\\App_Data\\nombres.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                nombre.Add(line);
+                counter++;
+            }
+
+            file.Close();
+
+
+
+            for (int i = 0; i < apellidos.Count; i++)
+            {
+                for (int j = 0; j < nombre.Count; j++)
+                {
+                    Student s = new Student();
+                    s.FirstName = (String)nombre[j];
+                    s.LastName = (String)apellidos[i];
+                    s.Email = "a@a.com";
+                    s.City = "Jose C Paz";
+                    s.DateOfBirth = DateTime.Now;
+                    s.Gender = "Masculino";
+
+                    StudentBusiness.Instance.Add(s);
+                }
+            }
+        }
+
+    }
+}
---
+++ PCyP.WebSite/Controllers/GestorController.cs
@@ -0,0 +1,33 @@
+﻿using PCyP.Domain.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PCyP.WebSite.Controllers
+{
+    public class GestorController : Controller
+    {
+        // GET: Gestor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int? id)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                CargarDatos.LeerArchivo();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
